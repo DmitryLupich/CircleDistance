60510ad6c1480f65b56c1bf6f2aac1f07dba4cb8
Dmytriy <dmytro.lupych@sixt.com>
2020-06-26 18:08:45 +0300

    Initial Commit

---
+++ CircleDistance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleDistance
+//
+//  Created by Dmitriy Lupych on 26.06.2020.
+//  Copyright © 2020 Dmitry Lupich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
